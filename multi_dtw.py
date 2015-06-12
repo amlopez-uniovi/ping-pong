7393e1948d8bc14e6ffd2523317321ae2365e83d
amlopez-uniovi <amlopez@MacBook-Pro-de-Antonio.local>
2015-06-12 15:18:26 +0200

    Arreglado problema en multi_dtw.py
    Estaba mal la gestión de variables globales así como no se usaba la
    función de distancia en la llamada a dtw.

--- multi_dtw.py
+++ multi_dtw.py
@@ -10,14 +10,22 @@ import numpy as np
 import sklearn.preprocessing as prep
 
 
-global sen1
-global sen2
+sen1 = []
+sen2 = []
 
 def my_custom_norm(x, y):
-    return np.linalg.norm(sen1[:,x]-sen2[:,x])
+    global sen1
+    global sen2
+    
+    #print(sen1)
+    #print(sen2)
+    return np.linalg.norm(sen1[:,x]-sen2[:,y])
     
 
 def multi_dtw(a, b, normalize = True):
+    global sen1
+    global sen2
+
     sen1 = a
     sen2 = b
     
@@ -28,5 +36,5 @@ def multi_dtw(a, b, normalize = True):
     indexa = np.arange(a.shape[1])    
     indexb = np.arange(b.shape[1])    
     
-    return dtw(indexa, indexb)
+    return dtw(indexa, indexb, dist = my_custom_norm)
     
